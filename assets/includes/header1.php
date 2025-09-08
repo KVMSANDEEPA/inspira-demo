5a8406b61aeb6988eeaf9cccf596c9fba3cfb170
KVMSAANDEEPA <malithsandeepa1081@gmail.com>
2025-09-08 22:22:44 +0530

    Update header1.php

--- assets/includes/header1.php
+++ assets/includes/header1.php
@@ -9,6 +9,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?? "My Website"; ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+     <link rel="icon" href="assets/img/favicon/favicon.ico" type="image/x-icon">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon-16x16.webp">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.webp">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-touch-icon.webp">
+    <link rel="android-chrome-icon" sizes="192x192" href="assets/img/favicon/android-chrome-192x192.webp">
+    <link rel="android-chrome-icon" sizes="512x512" href="assets/img/favicon/android-chrome-512x512.webp">
     <style>
         * {
             margin: 0;
